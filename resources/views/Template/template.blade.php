5b6028f0689be831e6a81a2ac36ca76e2ee3f2a0
Lamabuatnama <msyahdan@smkn1cianjur.sch.id>
2022-01-31 14:50:28 +0700

    Menerapkan Template

---
+++ resources/views/Template/template.blade.php
@@ -0,0 +1,28 @@
+@include('Template.atas')
+<!-- Site wrapper -->
+
+  <!-- Navbar -->
+@include('template.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+ @include('template.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+       @yield('content')
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<!-- ./wrapper -->
+@include('template.bawah')
+

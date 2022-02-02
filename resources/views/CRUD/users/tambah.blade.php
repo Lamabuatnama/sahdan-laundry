fdeee956b8b552b09f0e63d45162014200618c00
Lamabuatnama <msyahdan@smkn1cianjur.sch.id>
2022-02-02 15:00:56 +0700

    REGISTER USERS

--- resources/views/CRUD/users/tambah.blade.php
+++ resources/views/CRUD/users/tambah.blade.php
@@ -7,7 +7,7 @@
 <div class="modal-dialog lg" role="document">
   <div class="modal-content">
     <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel">MEMBER</h5>
+      <h5 class="modal-title" id="exampleModalLabel">TAMBAH USER</h5>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
@@ -15,7 +15,6 @@
     <div class="modal-body">
         <div class="card">
             <div class="card-body register-card-body">
-              <p class="login-box-msg">Register a new membership</p>
               <form action="" method="post">
                 @csrf
                 <div class="input-group mb-3">
@@ -70,12 +69,6 @@
                   </div>
                 <div class="row">
                   <div class="col-8">
-                    <div class="icheck-primary">
-                      <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-                      <label for="agreeTerms">
-                       I agree to the <a href="#">terms</a>
-                      </label>
-                    </div>
                   </div>
                   <!-- /.col -->
                   <div class="col-4">
@@ -84,7 +77,6 @@
                   <!-- /.col -->
                 </div>
               </form>
-              <a href="login" class="text-center">I already have a membership</a>
             </div>
             <!-- /.form-box -->
           </div><!-- /.card -->
